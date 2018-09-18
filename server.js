c572566c956dead113de3781139c35be1f34d062
NasibNia <nasibnia@gmail.com>
2018-09-17 21:58:23 -0700

    stylesheet

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,7 +20,7 @@ require("./app/routing/apiRouts")(app);
 require("./app/routing/htmlRouts")(app);
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
